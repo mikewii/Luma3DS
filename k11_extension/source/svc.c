eb27966b66dcaf914f67a04a567951c2bb5420d5
TuxSH <1922548+TuxSH@users.noreply.github.com>
2020-07-07 23:27:15 +0100

    kext: add hid/ir thread locking

--- k11_extension/source/svc.c
+++ k11_extension/source/svc.c
@@ -90,7 +90,7 @@ void signalSvcReturn(u8 *pageEnd)
 void postprocessSvc(void)
 {
     KThread *currentThread = currentCoreContext->objectContext.currentThread;
-    if(!currentThread->shallTerminate && rosalinaThreadLockPredicate(currentThread))
+    if(!currentThread->shallTerminate && rosalinaThreadLockPredicate(currentThread, rosalinaState & 5))
         rosalinaRescheduleThread(currentThread, true);
 
     officialPostProcessSvc();
