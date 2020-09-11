3d4a1716b56cc53c6cceaf7d9bf30bad028521c7
mikewii <mikew007.07@gmail.com>
2020-09-11 18:30:51 +0300

    Revert "rosalina/sm: properly interact with ndm"
    This reverts commit a32c77957329db904808ae17b26fd0cb897eae83.

--- sysmodules/rosalina/source/minisoc.c
+++ sysmodules/rosalina/source/minisoc.c
@@ -16,11 +16,26 @@ static u32 socContextAddr = 0x08000000;
 static u32 socContextSize = 0x60000;
 static Handle miniSocHandle;
 static Handle miniSocMemHandle;
-static bool exclusiveStateEntered = false;
 
 bool miniSocEnabled = false;
 
 s32 _net_convert_error(s32 sock_retval);
+
+// To prevent ndm:u from disconnecting us
+static Result srvExtAddToNdmuWorkaroundCount(s32 count)
+{
+    Result ret = 0;
+    u32 *cmdbuf = getThreadCommandBuffer();
+
+    cmdbuf[0] = IPC_MakeHeader(0x1000,1,0);
+    cmdbuf[1] = (u32)count;
+
+    ret = svcSendSyncRequest(*srvGetSessionHandle());
+    if(ret != 0)
+        return ret;
+
+    return cmdbuf[1];
+}
 
 static Result SOCU_Initialize(Handle memhandle, u32 memsize)
 {
@@ -87,18 +102,9 @@ Result miniSocInit(void)
     ret = SOCU_Initialize(miniSocMemHandle, socContextSize);
     if(ret != 0) goto cleanup;
 
-    if (!exclusiveStateEntered)
-    {
-        ndmuInit();
-        ret = NDMU_EnterExclusiveState(NDM_EXCLUSIVE_STATE_INFRASTRUCTURE);
-        if (R_SUCCEEDED(ret))
-            ret = NDMU_LockState(); // prevents ndm from switching to StreetPass when the lid is closed
-        //ndmuExit();
-        exclusiveStateEntered = R_SUCCEEDED(ret);
-    }
-
-    svcKernelSetState(0x10000, 0x10);
+    svcKernelSetState(0x10000, 2);
     miniSocEnabled = true;
+    srvExtAddToNdmuWorkaroundCount(1);
 
     return 0;
 
@@ -126,6 +132,7 @@ cleanup:
 
 Result miniSocExitDirect(void)
 {
+    //if (miniSocRefCount != 0) __builtin_trap();
     Result ret = 0;
     u32 tmp;
 
@@ -140,18 +147,9 @@ Result miniSocExitDirect(void)
     svcControlMemory(&tmp, socContextAddr, socContextAddr, socContextSize, MEMOP_FREE, MEMPERM_DONTCARE);
     if(ret == 0)
     {
-        if (exclusiveStateEntered)
-        {
-            //ndmuInit();
-            ret = NDMU_UnlockState();
-            if (R_SUCCEEDED(ret))
-                ret = NDMU_LeaveExclusiveState();
-            ndmuExit();
-            exclusiveStateEntered = R_FAILED(ret);
-        }
-
         miniSocEnabled = false;
-        svcKernelSetState(0x10000, 0x10);
+        srvExtAddToNdmuWorkaroundCount(-1);
+        svcKernelSetState(0x10000, 2);
     }
     return ret;
 }
--- sysmodules/sm/source/notifications.c
+++ sysmodules/sm/source/notifications.c
@@ -10,11 +10,17 @@ This is part of 3ds_sm, which is licensed under the MIT license (see LICENSE for
 
 #include <stdatomic.h>
 
+static atomic_int ndmuWorkaroundCount;
+
 static bool isNotificationInhibited(const ProcessData *processData, u32 notificationId)
 {
-    (void)processData;
+    u32 pid = processData->pid;
     switch(notificationId)
     {
+        // Shell opened, shell closed
+        case 0x213:
+        case 0x214:
+            return pid == ndmuServicePid && atomic_load(&ndmuWorkaroundCount) > 0;
         default:
             return false;
     }
@@ -207,5 +213,12 @@ Result PublishToAll(u32 notificationId)
             return 0xD8606408;
     }
 
+    return 0;
+}
+
+Result AddToNdmuWorkaroundCount(s32 count)
+{
+    // Note: no check is made to (current value)+count
+    atomic_fetch_add(&ndmuWorkaroundCount, count);
     return 0;
 }
